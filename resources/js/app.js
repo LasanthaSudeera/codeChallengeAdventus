088cfa8ad186158597f8b61274baa64345683554
Lasantha <lasan.sudeera@gmail.com>
2022-06-09 12:28:26 +0530

    Added vForm package
    vFrom can simplify the form handling with laravel and vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,19 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Module Imports
+
+// DataTable
+Vue.component('data-table', require('./components/modules/DataTable.vue').default);
+
+// vForm
+import Form from 'vform';
+window.Form = Form;
+
+// UI Imports
+
+// User
+Vue.component('show-user-component', require('./components/main/user/ShowUserComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,8 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <show-user-component></show-user-component>
                 </div>
             </div>
         </div>
